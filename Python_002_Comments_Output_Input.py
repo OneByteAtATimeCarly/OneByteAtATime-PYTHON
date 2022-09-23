1210b9541fdb89deb87bfc7e9218ec69eb078b5a
Carly Salali Germany <112182514+OneByteAtATimeCarly@users.noreply.github.com>
2022-09-23 09:48:05 -0400

    Update Python_002_Comments_Output_Input.py

--- Python_002_Comments_Output_Input.py
+++ Python_002_Comments_Output_Input.py
@@ -1,4 +1,4 @@
-# Title: Python Comment tags, console output using the print() method and console input using the input() method: 
+# Title: Python Comment tags, console output using the print() method and console input using the input() method
 # Author: C. S. Germany 01/15/2022
 
 # Two ways to comment out code in Python
