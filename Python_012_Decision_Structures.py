7651a5ed24fb60fad18960832d66ca2872ad4742
Carly Salali Germany <112182514+OneByteAtATimeCarly@users.noreply.github.com>
2023-04-23 03:11:01 -0400

    Add files via upload

--- Python_012_Decision_Structures.py
+++ Python_012_Decision_Structures.py
@@ -6,12 +6,12 @@
 #   Basic if/else
 def Decision_Structures_0l():
     
-    The_Name = "Invader Zim";
+    The_Name = "Carly";
 
     if(The_Name == "Invader Zim"):
-       print("Good morning, sir!");
+       print("\nGood morning, sir!\n");
     else:
-       print("Intruder alert!");   
+       print("\nIntruder alert!\n");   
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------- 
 
 
@@ -23,9 +23,9 @@ def Decision_Structures_02():
     The_Score = 42;
 
     if(The_Score == 42):
-       print("The meaning of LIFE!");
+       print("\nThe meaning of LIFE!\n");
     else:
-       print("Void of any meaning or purpose.");   
+       print("\nVoid of any meaning or purpose.\n");   
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------- 
 
 
@@ -34,7 +34,11 @@ def Decision_Structures_02():
 #   Basic if/elif/else and using logical "and" and comparison operators <, >, <=, >=, ==
 def Decision_Structures_03():
     
-    Test_Scores = [65,85,97,45,100,63,88];
+#Logical Comparison Operators: AND and OR
+# AND = BOTH sides have to be true
+# OR Only ONE side has to be true
+
+    Test_Scores = [65,85,97,45,100,63,88,444];
 
     for z in Test_Scores:
         
@@ -62,7 +66,8 @@ def Decision_Structures_04():
     Score_1 = 42;
     Score_2 = 444;
 
-    if Score_1 > Score_2: print("Score_1 is greater than Score_2.");
+    if Score_1 > Score_2: print("\nScore_1 is greater than Score_2.\n");
+    else: print("\nScore 1 is NOT greater than Score 2.\n");
 
     print("A") if Score_1 > Score_2 else print("B");
 
@@ -78,13 +83,45 @@ def Decision_Structures_05():
     Score_1 = 42;
     Score_2 = 444;
 
-    if(Score_1 > 10 and Score_1 < 42):
-       print("Target condition A triggered!");
+    if(Score_1 > 10 and Score_1 <= 42):
+       print("\nTarget condition A triggered!");
 
     if(Score_1 > 10 or Score_1 < 42):
-       print("Target condition B triggered!");       
+       print("Target condition B triggered!\n");       
 
 
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+
+
+#6. Function-----------------------------------------------------------------------------------------------------------------------------------------------------
+#   Unlike C++, Java or PowerShell, there is no switch statement in Python
+# You can approximate a switch statement however using a series of simple one-line if statements.
+def Decision_Structures_06():
+    User_Input = "";
+
+    while(User_Input != "q"):
+          User_Input = input("\nWhat was your score? (q to quit) ");
+
+          if(User_Input != "q"):
+             
+             if(User_Input.isdigit()):
+                print("Numerical value entered. Let's proceed.");
+                print("Converting the STRING data to INT type.");
+                The_Score = int(User_Input); 
+    
+                print("\nYour grade is: ",end='');
+
+                if(The_Score < 65): print("F"); 
+                elif(The_Score >= 65 and The_Score < 70): print("D");
+                elif(The_Score >= 70 and The_Score < 80): print("C");
+                elif(The_Score >= 80 and The_Score < 90): print("B");
+                elif(The_Score >= 90 and The_Score <= 99): print("A");
+                elif(The_Score == 100): print("Perfect A+! 100%!");
+                elif(The_Score > 100): print("A++! Awesome!");
+
+                print("\n");
+             
+             else: print("That was NOT a number!");
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------- 
 
 
@@ -93,7 +130,9 @@ def Decision_Structures_05():
 #Decision_Structures_02();
 #Decision_Structures_03();
 #Decision_Structures_04();
-Decision_Structures_05();
+#Decision_Structures_05();
+Decision_Structures_06();
+
 
 
 
